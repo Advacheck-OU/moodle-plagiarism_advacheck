c2ad0b18519008809d0b796a3da7a64f16df0812
Dorofeev Eugene <162182786+dorofeev-ev96@users.noreply.github.com>
2024-12-02 18:02:26 +0300

    Update version.php

--- version.php
+++ version.php
@@ -26,4 +26,4 @@ $plugin->component = 'plagiarism_advacheck';
 $plugin->version = 2024112921;
 $plugin->requires = 2010042803;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.2.4';
+$plugin->release = '3.2.6';
