b32e60de4eb67a4d96fc4c5c55a6fc631b4a136f
Dorofeev Eugene <162182786+dorofeev-ev96@users.noreply.github.com>
2025-07-28 16:45:40 +0300

    Update version.php

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'plagiarism_advacheck';
-$plugin->version = 2025021818;
+$plugin->version = 2025021819;
 $plugin->requires = 2010042803;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '3.3.1';
